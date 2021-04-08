9d3f84d0a439444736d950213ee6b69d1fa6c7bd
g.severgin <g.severgin@prosoftsystems.ru>
2021-04-08 19:26:43 +0500

    Чт 08 апр 2021 19:26:43 +05

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ std::vector<std::string> split(const std::string& str, char d)
 
     std::string::size_type start = 0;
     std::string::size_type stop = str.find_first_of(d);
-    while(stop != std::string::npos)
+    while (stop != std::string::npos)
     {
         r.emplace_back(str.substr(start, stop - start));
 
@@ -33,102 +33,109 @@ std::vector<std::string> split(const std::string& str, char d)
     return r;
 }
 
+typedef std::vector<std::vector<std::string>> IpPool;
+typedef std::vector<std::string> StringsVector;
+
+void print(const StringsVector& strings)
+{
+    for (auto ip_part = strings.cbegin(); ip_part != strings.cend(); ++ip_part)
+    {
+        if (ip_part != strings.cbegin())
+        {
+            std::cout << ".";
+        }
+        std::cout << *ip_part;
+    }
+}
+
 int main(int argc, char const *argv[])
 {
-	std::cout << "Build: " << version() << std::endl;
+    std::cout << "Build: " << version() << std::endl;
     try
     {
-    	if (argc > 1)
+        if (argc > 1)
         {
-        	std::ifstream input(argv[1]);
+            std::ifstream input(argv[1]);
 
-	        std::vector<std::vector<std::string>> ip_pool;
+            IpPool ip_pool;
 
-	        for (std::string line; std::getline(input, line);)
-	        {
-	            std::vector<std::string> v = split(line, '\t');
-	            ip_pool.emplace_back(split(v.at(0), '.'));
-	        }
+            for (std::string line; std::getline(input, line);)
+            {
+                std::vector<std::string> v = split(line, '\t');
+                ip_pool.emplace_back(split(v.at(0), '.'));
+            }
 
-	        std::sort(ip_pool.begin(), ip_pool.end(), std::greater<>());
+            std::sort(ip_pool.begin(), ip_pool.end(), std::greater<>());
 
-	        for (std::vector<std::vector<std::string>>::const_iterator ip = ip_pool.cbegin(); ip != ip_pool.cend(); ++ip)
-	        {
-	            for (std::vector<std::string>::const_iterator ip_part = ip->cbegin(); ip_part != ip->cend(); ++ip_part)
-	            {
-	                if (ip_part != ip->cbegin())
-	                {
-	                    std::cout << ".";
+            for (auto ip = ip_pool.cbegin(); ip != ip_pool.cend(); ++ip)
+            {
+                print(*ip);
+                std::cout << std::endl;
+            }
 
-	                }
-	                std::cout << *ip_part;
-	            }
-	            std::cout << std::endl;
-	        }
+            // 222.173.235.246
+            // 222.130.177.64
+            // 222.82.198.61
+            // ...
+            // 1.70.44.170
+            // 1.29.168.152
+            // 1.1.234.8
 
-	        // 222.173.235.246
-	        // 222.130.177.64
-	        // 222.82.198.61
-	        // ...
-	        // 1.70.44.170
-	        // 1.29.168.152
-	        // 1.1.234.8
+            // TODO filter by first byte and output
+            // ip = filter(1)
 
-	        // TODO filter by first byte and output
-	        // ip = filter(1)
+            // 1.231.69.33
+            // 1.87.203.225
+            // 1.70.44.170
+            // 1.29.168.152
+            // 1.1.234.8
 
-	        // 1.231.69.33
-	        // 1.87.203.225
-	        // 1.70.44.170
-	        // 1.29.168.152
-	        // 1.1.234.8
+            // TODO filter by first and second bytes and output
+            // ip = filter(46, 70)
 
-	        // TODO filter by first and second bytes and output
-	        // ip = filter(46, 70)
+            // 46.70.225.39
+            // 46.70.147.26
+            // 46.70.113.73
+            // 46.70.29.76
 
-	        // 46.70.225.39
-	        // 46.70.147.26
-	        // 46.70.113.73
-	        // 46.70.29.76
+            // TODO filter by any byte and output
+            // ip = filter_any(46)
 
-	        // TODO filter by any byte and output
-	        // ip = filter_any(46)
-
-	        // 186.204.34.46
-	        // 186.46.222.194
-	        // 185.46.87.231
-	        // 185.46.86.132
-	        // 185.46.86.131
-	        // 185.46.86.131
-	        // 185.46.86.22
-	        // 185.46.85.204
-	        // 185.46.85.78
-	        // 68.46.218.208
-	        // 46.251.197.23
-	        // 46.223.254.56
-	        // 46.223.254.56
-	        // 46.182.19.219
-	        // 46.161.63.66
-	        // 46.161.61.51
-	        // 46.161.60.92
-	        // 46.161.60.35
-	        // 46.161.58.202
-	        // 46.161.56.241
-	        // 46.161.56.203
-	        // 46.161.56.174
-	        // 46.161.56.106
-	        // 46.161.56.106
-	        // 46.101.163.119
-	        // 46.101.127.145
-	        // 46.70.225.39
-	        // 46.70.147.26
-	        // 46.70.113.73
-	        // 46.70.29.76
-	        // 46.55.46.98
-	        // 46.49.43.85
-	        // 39.46.86.85
-	        // 5.189.203.46
-    	}
+            // 186.204.34.46
+            // 186.46.222.194
+            // 185.46.87.231
+            // 185.46.86.132
+            // 185.46.86.131
+            // 185.46.86.131
+            // 185.46.86.22
+            // 185.46.85.204
+            // 185.46.85.78
+            // 68.46.218.208
+            // 46.251.197.23
+            // 46.223.254.56
+            // 46.223.254.56
+            // 46.182.19.219
+            // 46.161.63.66
+            // 46.161.61.51
+            // 46.161.60.92
+            // 46.161.60.35
+            // 46.161.58.202
+            // 46.161.56.241
+            // 46.161.56.203
+            // 46.161.56.174
+            // 46.161.56.106
+            // 46.161.56.106
+            // 46.101.163.119
+            // 46.101.127.145
+            // 46.70.225.39
+            // 46.70.147.26
+            // 46.70.113.73
+            // 46.70.29.76
+            // 46.55.46.98
+            // 46.49.43.85
+            // 39.46.86.85
+            // 5.189.203.46
+        }
     }
     catch(const std::exception& e)
     {
