80c50549a145e90182e94e265aede8ee9bd0e930
g.severgin <g.severgin@prosoftsystems.ru>
2021-05-03 12:57:48 +0500

    Пн 03 мая 2021 12:57:48 +05

--- test_version.cpp
+++ test_version.cpp
@@ -14,27 +14,27 @@ BOOST_AUTO_TEST_CASE(test_valid_version)
     BOOST_CHECK(version() > 0);
 }
 
-BOOST_AUTO_TEST_CASE(add_and_sort_test)
-{
-    ip_filter::Filter filter;
-    filter.Add(ip_filter::split("10.1.31.71", '.'));
-    filter.Add(ip_filter::split("1.1.52.188", '.'));
-    filter.Add(ip_filter::split("1.1.51.189", '.'));
-    filter.Add(ip_filter::split("2.5.64.21", '.'));
-    filter.Add(ip_filter::split("2.5.64.23", '.'));
-    filter.Add(ip_filter::split("3.7.8.251", '.'));
+// BOOST_AUTO_TEST_CASE(add_and_sort_test)
+// {
+//     ip_filter::Filter filter;
+//     filter.Add(ip_filter::split("10.1.31.71", '.'));
+//     filter.Add(ip_filter::split("1.1.52.188", '.'));
+//     filter.Add(ip_filter::split("1.1.51.189", '.'));
+//     filter.Add(ip_filter::split("2.5.64.21", '.'));
+//     filter.Add(ip_filter::split("2.5.64.23", '.'));
+//     filter.Add(ip_filter::split("3.7.8.251", '.'));
 
-    filter.Sort();
+//     filter.Sort();
 
-    std::ostringstream os;
+//     std::ostringstream os;
 
-    filter.PrintAll(os);
+//     filter.PrintAll(os);
 
-    const StringsVector values(ip_filter::split(os.str(), '\n'));
+//     const StringsVector values(ip_filter::split(os.str(), '\n'));
 
-    const StringsVector sample = {"1.1.51.189", "1.1.52.188", "2.5.64.21", "2.5.64.23", "3.7.8.251", "10.1.31.71"};
+//     const StringsVector sample = {"1.1.51.189", "1.1.52.188", "2.5.64.21", "2.5.64.23", "3.7.8.251", "10.1.31.71"};
 
-    BOOST_CHECK(sample == values);
-}
+//     BOOST_CHECK(sample == values);
+// }
 
 }
