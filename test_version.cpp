610021b008e81de603f5a4c008c8624fa342571d
g.severgin <g.severgin@prosoftsystems.ru>
2021-05-03 13:15:21 +0500

    Пн 03 мая 2021 13:15:21 +05

--- test_version.cpp
+++ test_version.cpp
@@ -5,7 +5,6 @@
 #include <boost/test/unit_test.hpp>
 
 #include "lib.h"
-#include "ip_filter.h"
 
 BOOST_AUTO_TEST_SUITE(test_version)
 
@@ -13,28 +12,5 @@ BOOST_AUTO_TEST_CASE(test_valid_version)
 {
     BOOST_CHECK(version() > 0);
 }
-
-// BOOST_AUTO_TEST_CASE(test_add_and_sort)
-// {
-//     ip_filter::Filter filter;
-//     filter.Add(ip_filter::split("10.1.31.71", '.'));
-//     filter.Add(ip_filter::split("1.1.52.188", '.'));
-//     filter.Add(ip_filter::split("1.1.51.189", '.'));
-//     filter.Add(ip_filter::split("2.5.64.21", '.'));
-//     filter.Add(ip_filter::split("2.5.64.23", '.'));
-//     filter.Add(ip_filter::split("3.7.8.251", '.'));
-
-//     filter.Sort();
-
-//     std::ostringstream os;
-
-//     filter.PrintAll(os);
-
-//     const StringsVector values(ip_filter::split(os.str(), '\n'));
-
-//     const StringsVector sample = {"1.1.51.189", "1.1.52.188", "2.5.64.21", "2.5.64.23", "3.7.8.251", "10.1.31.71"};
-
-//     BOOST_CHECK(sample == values);
-// }
 
 BOOST_AUTO_TEST_SUITE_END()
