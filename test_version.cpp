bc63ddeb5525e324d1f151220736334394bb7701
g.severgin <g.severgin@prosoftsystems.ru>
2021-05-03 13:24:27 +0500

    Пн 03 мая 2021 13:24:27 +05

--- test_version.cpp
+++ test_version.cpp
@@ -1,18 +1,35 @@
-#pragma once
-
 #define BOOST_TEST_MODULE test_version
-
-#include <sstream>
 
 #include <boost/test/unit_test.hpp>
 
+#include <sstream>
 #include "lib.h"
-
-BOOST_AUTO_TEST_SUITE(test_version)
+#include "ip_filter.h"
 
 BOOST_AUTO_TEST_CASE(test_valid_version)
 {
     BOOST_CHECK(version() > 0);
 }
 
-BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_CASE(test_add_and_sort)
+{
+    ip_filter::Filter filter;
+    filter.Add(ip_filter::split("10.1.31.71", '.'));
+    filter.Add(ip_filter::split("1.1.52.188", '.'));
+    filter.Add(ip_filter::split("1.1.51.189", '.'));
+    filter.Add(ip_filter::split("2.5.64.21", '.'));
+    filter.Add(ip_filter::split("2.5.64.23", '.'));
+    filter.Add(ip_filter::split("3.7.8.251", '.'));
+
+    filter.Sort();
+
+    std::ostringstream os;
+
+    filter.PrintAll(os);
+
+    const StringsVector values(ip_filter::split(os.str(), '\n'));
+
+    const StringsVector sample = {"1.1.51.189", "1.1.52.188", "2.5.64.21", "2.5.64.23", "3.7.8.251", "10.1.31.71"};
+
+    BOOST_CHECK(sample == values);
+}
